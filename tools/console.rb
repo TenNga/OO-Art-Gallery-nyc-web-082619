af809986aec780b8290fff40dcfdb6b77cbf177c
Kevin Karma <kevinkarma55@gmail.com>
2019-09-02 21:52:57 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,12 @@
 require_relative '../config/environment.rb'
 
-kevin = Artist.new()
+kevin = Artist.new("Kevin", 23)
+karma = Artist.new("Karma", 10)
+tenzin = Artist.new("Tenzin", 7)
+
+sidePoint = Gallary.new()
+
+kevin.create_painting("NYC subway",30200,)
 
 
 
