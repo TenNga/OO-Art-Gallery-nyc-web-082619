c596f88519afd993d4a9f2047180a3a14bde1daa
Kevin Karma <kevinkarma55@gmail.com>
2019-09-02 21:58:02 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,9 @@ kevin = Artist.new("Kevin", 23)
 karma = Artist.new("Karma", 10)
 tenzin = Artist.new("Tenzin", 7)
 
-sidePoint = Gallary.new("Side Point", "New York City")
-fiveStarStudio = Gallary.new("Five Star Studio", "L.A.")
-feelIt = Gallary.new("Feel IT", "Boston")
+sidePoint = Gallery.new("Side Point", "New York City")
+fiveStarStudio = Gallery.new("Five Star Studio", "L.A.")
+feelIt = Gallery.new("Feel IT", "Boston")
 
 kevin.create_painting("NYC subway",30200,sidePoint)
 kevin.create_painting("Top of World", 1500,fiveStarStudio)
